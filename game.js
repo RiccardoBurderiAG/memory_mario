4ad030e2d124a74cbd9881b3adc922917fa0cc9e
Riccardo Burderi <riccardo.burderi@gmail.com>
2021-02-03 12:28:11 +0100

    Dynamic layout for game page (cards number based on localStorage)

--- game.js
+++ game.js
@@ -56,9 +56,27 @@ window.onload = function(){
     /* in questa funzione facciamo in modo che venga preso solo una porzione di cards e lo mescoliamo e poi creiamo i div */
     let slicedCards = cards.slice(0,levelCard);
     shuffleCards(slicedCards);
+    //per ogni card dentro lo slicedCards (mazzo già mescolato) creiamo l elemento e mettiamolo nella grid => <div class="card">ciao</div>
+    let gridCards = document.querySelector(".cardGame");
+    slicedCards.map(s=>{
+        let gridElement = document.createElement("div");
+        let gridInner = document.createTextNode("ciao");
+        gridElement.appendChild(gridInner);
+        gridElement.className = "card";
+        gridElement.id = s.cardId;
+        console.log(gridElement);
+        gridCards.appendChild(gridElement);
+    });
+    /* let gridCards = document.getElementsByClassName(".cardGame");
+    let gridElement = document.createElement("div")
+    gridElement.className = "card";
+    gridCards.appendChild(gridElement); */
 }
 
 
 function shuffleCards(array){
     console.log("sto mescolando il mazzo", array);
+    //al momento non fa nulla
+    var shuffledDeck = array;
+    return shuffledDeck;
 }
