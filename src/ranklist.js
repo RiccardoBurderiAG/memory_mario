b846631245ca2f16a8a56f99aa971daac7aa7177
Riccardo Burderi <riccardo.burderi@gmail.com>
2021-02-10 12:30:51 +0100

    completed mockup

--- src/ranklist.js
+++ src/ranklist.js
@@ -6,6 +6,30 @@ let playersScore = getLocalStorage("savedGames");
 
 window.onload = function(){
     let tableBody = document.getElementsByClassName("ranklist");
+    let center = document.getElementsByClassName("center");
+    let title = document.createElement("h1");
+    switch (lvl) {
+        case 12:
+            title.innerHTML += " 1" + ` (`+lvl+`)`;
+            break;
+        case 16:
+            title.innerHTML += " 2" +` (`+lvl+`)`;
+            break;
+        case 20:
+            title.innerHTML += " 3" +` (`+lvl+`)`;
+             break;
+        case 24:
+            title.innerHTML += " 4" +` (`+lvl+`)`;
+             break;
+        case 30:
+            title.innerHTML += " 5" +` (`+lvl+`)`;
+             break;
+        case 36:
+            title.innerHTML += " 6" +` (`+lvl+`)`;
+             break;
+        default:
+            break;
+    }
     console.log(tableBody[0]);
     playersScore.map(p=>{
         if(p.level === lvl){
